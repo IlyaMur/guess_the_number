d73967300d039a4787905083ec147baa65a85186
Ilya Mur <ilyamur@hotmail.com>
2021-08-05 15:34:05 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,38 +1,67 @@
 'use strict';
 
+// Проверяем на число
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-alert('Добро пожаловать в игру "Угадай число"!');
+// Печатаем сообщения
+function printMessage(message) {
+  alert(message);
+}
 
+// Проверяем на null
+function isUserAnswerNull(userGuess) {
+  return userGuess === null;
+}
+
+// Принимаем ввод пользователя
+function askUserInput() {
+  return prompt('Угадай число от 1 до 100:');
+}
+
+// Рекурсивно проверяем, что ввёл пользователь. Возвращаем:
+//    undefined - в случае null
+//    число - если всё ок
+function checkUserInputRecursively(userGuess) {
+  if (isUserAnswerNull(userGuess)) {
+    return;
+  } else if (isNumber(userGuess)) {
+    return userGuess;
+  }
+
+  printMessage('Введи число!');
+  userGuess = askUserInput();
+
+  checkUserInputRecursively(userGuess);
+}
+
+
+printMessage('Добро пожаловать в игру "Угадай число"!');
+
+// Основная функция - загадывает число от 0 - 100.
+// Рекурсивно вызывает askRecursively
 function guessNumber() {
   let number = Math.ceil(Math.random() * 100);
 
+  // Запрашивает ввод и проверяет его.
+  // В случае 'cancel' завершается и говорит число.
   function askRecursively() {
-    let guess = prompt('Угадай число от 1 до 100:');
+    let userGuess = askUserInput();
 
-    if (guess === null) {
-      alert(`Игра окончена. Загаданное число было: ${number}`);
+    userGuess = checkUserInputRecursively(userGuess);
+
+    if (!userGuess) {
+      printMessage(`Игра окончена. Загаданное число было: ${number}`);
       return;
     }
 
-    function checkInputRecursively() {
-      if (isNumber(guess)) { return; }
-      alert('Введи число!');
-      guess = prompt('Угадай число от 1 до 100:');
-
-      checkInputRecursively();
-    }
-
-    checkInputRecursively();
-
-    if (guess < number) {
-      alert('Не угадал. Загаданное число больше');
-    } else if (guess > number) {
-      alert('Не угадал. Загаданное число меньше');
+    if (userGuess < number) {
+      printMessage('Не угадал. Загаданное число больше');
+    } else if (userGuess > number) {
+      printMessage('Не угадал. Загаданное число меньше');
     } else {
-      alert('Поздравляю, Вы угадали!!!');
+      printMessage('Поздравляю, Вы угадали!!!');
       return;
     }
 
